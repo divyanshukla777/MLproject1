aa8bd093a9c08698fb42047d1c1a797037650eb5
Razor101 <aviralsingh99@gmail.com>
2020-09-15 19:20:35 +0530

    upgrade

--- spamator/views.py
+++ spamator/views.py
@@ -4,3 +4,8 @@ from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def checksms(request):
+    sms=request.POST['smstext']
+    messages.success(request,'0')
+    return redirect('index')
